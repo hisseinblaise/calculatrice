78ba8512bf57a1ac5b06e3c464fdbfe8b21feab8
hisseinblaise <hisseinblaise7@gmail.com>
2025-08-19 11:43:36 +0100

    feat: add operation fonctionnality

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
- function calculerMarge() {
-      const semences = Number(document.getElementById("semences").value) || 0;
-      const engrais = Number(document.getElementById("engrais").value) || 0;
-      const main = Number(document.getElementById("main").value) || 0;
-      const autres = Number(document.getElementById("autres").value) || 0;
-      const vente = Number(document.getElementById("vente").value) || 0;
+function calculerMarge() {
+  const semences = Number(document.getElementById("semences").value) || 0;
+  const engrais = Number(document.getElementById("engrais").value) || 0;
+  const main = Number(document.getElementById("main").value) || 0;
+  const autres = Number(document.getElementById("autres").value) || 0;
+  const vente = Number(document.getElementById("vente").value) || 0;
 
-      const coutTotal = semences + engrais + main + autres;
-      const marge = vente - coutTotal;
+  const depenseTotal = semences + engrais + main + autres;
+  const marge = vente - depenseTotal;
 
-      let message = `Votre marge est de ${marge} FCFA.`;
-      if (marge < 0) {
-        message += " Attention, vous êtes en perte.";
-      }
-      document.getElementById("resultat").textContent = message;
-    }
+  let message = `Votre marge est de: ${marge} FCFA.`;
+  if (marge < 0) {
+    message += " Attention, vous êtes en perte.";
+    document.getElementById("resultat").style.color = "red";
+    document.getElementById("resultat").style.fontSize = "30px";
+  } else if (marge ===0) {
+    message += "Vous êtes en équilibre.";
+    document.getElementById("resultat").style.color = "green";
+    document.getElementById("resultat").style.fontSize = "25px";
+  } 
+    document.getElementById("resultat").textContent = message;
+    // document.getElementById("resultat").style.color = "green";
+    // document.getElementById("resultat").style.fontSize = "25px";
+}
